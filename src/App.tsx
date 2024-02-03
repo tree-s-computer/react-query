24d67193aac988a1516ecfd99bb4173a3e5318b8
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-03 22:49:41 +0900

    feat: add devtool

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { QueryClient, useQuery } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -8,16 +13,13 @@ function fetchData() {
 }
 
 function TestComponent() {
-  const { data, isFetching } = useQuery(
-    {
-      queryKey: ['number'],
-      queryFn: fetchData,
-      //refetchInterval: 1000,
-      staleTime: 3000, // 5 seconds stale time
-      gcTime: 10000, // 30 seconds cache time
-    },
-    queryClient
-  );
+  const { data, isFetching } = useQuery({
+    queryKey: ['number'],
+    queryFn: fetchData,
+    //refetchInterval: 1000,
+    staleTime: 3000, // 5 seconds stale time
+    gcTime: 10000, // 30 seconds cache time
+  });
 
   if (isFetching) return <div>Loading...</div>;
 
@@ -28,12 +30,15 @@ function App() {
   const [showComponent, setShowComponent] = React.useState(true);
 
   return (
-    <div>
-      <button onClick={() => setShowComponent(!showComponent)}>
-        Toggle Component
-      </button>
-      {showComponent && <TestComponent />}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <button onClick={() => setShowComponent(!showComponent)}>
+          Toggle Component
+        </button>
+        {showComponent && <TestComponent />}
+      </div>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
 
